2f014b39f9877f5cb2ffbd04c8bf1d288b842586
mdirfan16 <mdirfan.am16@gmail.com>
2024-09-23 23:07:02 +0600

    added function in first div input

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-document.getElementById('donate-button').addEventListener('click', function () {
+document.getElementById('donate-button').addEventListener('click', function (event) {
+   event.preventDefault()
 
-   const inputNumber = parseFloat(document.getElementById('donate-amount').value)
-   const balance = parseFloat(document.getElementById('fixed-balance').innerText)
+   const inputNumber = parseFloat(document.getElementById('donation-amount').value)
+   // const balance = parseFloat(document.getElementById('fixed-balance').innerText)
+   const newBalance = parseFloat(document.getElementById('donate-new-balance').innerText)
    if (inputNumber === 'number') {
 
-
-      const newBalance = parseFloat(document.getElementById('donate-new-balance').innerText)
       const perfectBalance = balance - inputNumber
+      const donateBalance = newBalance + inputNumber
       document.getElementById('fixed-balance').innerText = perfectBalance
+
+      document.getElementById('fixed-balance').innerText = donateBalance
    }
    else if (inputNumber !== 'number' && inputNumber === 'string' && inputNumber < 0){
       alert ('invalid Input ! please put a valid amount')
